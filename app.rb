9dc206cb4bfcbf4428c0d8cd3496b349e1fd94f8
Harman Singh <harman28@gmail.com>
2017-05-25 08:17:42 +0530

    Rubocop-suggested changes

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ require 'yaml'
 
 def get_url(domain_object, rel_route)
   if domain_object.is_a? Hash
-    return domain_object['root'] + "/" + rel_route
+    domain_object['root'] + '/' + rel_route
   elsif domain_object.is_a? String
-    return domain_object
+    domain_object
   end
 end
 
@@ -15,14 +15,13 @@ get '/*' do
   hostname = request.host
   route = params[:splat][0]
 
-  YAML::load_file('redirects.yml').each do |code, zone|
-    if zone.has_key? hostname
-      url = get_url(zone[hostname], route)
-      if url
-        redirect url, code
-      else
-        halt 400, "Invalid configuration for #{hostname}"
-      end
+  YAML.load_file('redirects.yml').each do |code, zone|
+    next unless zone.key? hostname
+    url = get_url(zone[hostname], route)
+    if url
+      redirect url, code
+    else
+      halt 400, "Invalid configuration for #{hostname}"
     end
   end
 
